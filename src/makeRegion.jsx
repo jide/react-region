abf0d6e0c0e810afe661e5914d2cf06d7c305b34
jide <jide.fr@gmail.com>
2015-09-10 22:02:55 +0200

    Add a class name to container component

--- src/makeRegion.jsx
+++ src/makeRegion.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Region from './Region';
 
 export default (types = {}) => {
-  return class extends Component {
+  return class RegionContainer extends Component {
     render() {
       return <Region types={ types } { ...this.props }/>;
     }
