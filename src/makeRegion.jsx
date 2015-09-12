aa69300120f3826ceb8cfe96737b575848e8f454
Julien De Luca <jide.fr@gmail.com>
2015-09-12 22:54:05 +0200

    Optionally pass a custom Component to makeRegion

--- src/makeRegion.jsx
+++ src/makeRegion.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Region from './Region';
 
-export default (types = {}) => {
+export default (types = {}, RegionComponent = Region) => { console.log('ttttt');
   return class RegionContainer extends Component {
     render() {
-      return <Region types={ types } { ...this.props }/>;
+      return <RegionComponent types={ types } { ...this.props }/>;
     }
   };
 };
