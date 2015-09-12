3e304759c35bbb7f8b7f835191055dc1e6715d61
Julien De Luca <jide.fr@gmail.com>
2015-09-12 23:11:05 +0200

    Fix typo

--- src/makeRegion.jsx
+++ src/makeRegion.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Region from './Region';
 
-export default (types = {}, RegionComponent = Region) => { console.log('ttttt');
+export default (types = {}, RegionComponent = Region) => {
   return class RegionContainer extends Component {
     render() {
       return <RegionComponent types={ types } { ...this.props }/>;
